8e9fb947f02ef0ed0c7124232d1738fab6e5b6f3
HristomirAsenov <hristomir.asenov@gmail.com>
2020-11-05 12:48:40 +0200

    initial testing commit

---
+++ index.js
@@ -0,0 +1,19 @@
+document.querySelector('button').addEventListener('click', fetchSoftUniPartners);
+
+function fetchSoftUniPartners() {
+    const URL = "https://softuni.bg/partners";
+
+    fetch(URL)
+        .then((r) => r.text())
+        .then(appendSoftUniPartners);
+}
+
+function appendSoftUniPartners(htmlResponse) {
+    let div = document.createElement('div');
+    div.innerHTML = htmlResponse;
+
+    document.querySelector('div#result').textContent = JSON.stringify(
+        Array.from(div.querySelectorAll('#diamond-partners li a.company-list'))
+            .map(e => { return { title: e.title, companyImageUrl: e.href } })
+    );
+}
